4219b2441016a12e51f9405ed2882271b73ef1cd
Dabronzo <ayrtonterceiro@gmail.com>
2022-05-15 15:40:51 +0000

    Created the first views

--- agenda/views.py
+++ agenda/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from django.shortcuts import get_object_or_404, redirect
+from django.views import View
 
-# Create your views here.
+
+class LandingGigsView(View):
+    """View class to handle the gigs list or the landing page"""
+
+    def get(self, request, *args, **kwargs):
+
+        logged_in = self.request.user
+        if logged_in.id is None:
+            return render(
+                request, 'landing.html'
+                )
+        else:
+            return render(
+                request, 'index.html'
+            )
+
+
