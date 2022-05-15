e138467c2e960b7fad2a2e0cce6eecf3d4504e02
Dabronzo <ayrtonterceiro@gmail.com>
2022-05-15 14:41:41 +0000

    Added models for gig and venues

--- agenda/admin.py
+++ agenda/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import Gig
+from .models import Venue
 
-# Register your models here.
+
+@admin.register(Gig)
+class GigAdminManager(admin.ModelAdmin):
+    """Class for gig at the admin page"""
+
+    list_display = ('name', 'date', 'dj', 'venue', 'status')
+    prepopulated_fields = {'slug': ('name',)}
+
+
+@admin.register(Venue)
+class VenueAdminManager(admin.ModelAdmin):
+    """class for venues at the admin page"""
+
+    list_display = ('name', 'address', 'city', 'contact')
+    prepopulated_fields = {'slug': ('name',)}
--- agenda/models.py
+++ agenda/models.py
@@ -1,3 +1,57 @@
 from django.db import models
+from accounts.models import NewDjUser
+from datetime import date
 
-# Create your models here.
+STATUS = ((0, 'Proposal'), (1, 'Aproved'), (2, 'Rejected'), (3, 'Cancelled'))
+
+
+class Venue(models.Model):
+    """Class table for the venues"""
+
+    name = models.CharField('Venue Name', max_length=120, unique=True)
+    address = models.CharField(max_length=200)
+    city = models.CharField('City', max_length=120)
+    website = models.URLField('Venue Website')
+    contact = models.CharField('Contact Name and Phone', max_length=200)
+    additional_info = models.TextField(blank=True)
+    slug = models.SlugField(max_length=200, unique=True)
+
+    def __str__(self):
+        return f"{self.name} located at {self.address}, {self.city}. Contact {self.contact}"
+
+
+class Gig(models.Model):
+    """Class table for the gigs"""
+
+    name = models.CharField(max_length=120, unique=True)
+    slug = models.SlugField(max_length=200, unique=True)
+    status = models.IntegerField(choices=STATUS, default=0)
+    date = models.DateField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
+    time_start = models.TimeField()
+    time_duration = models.TimeField()
+    dj = models.ForeignKey(
+        NewDjUser, on_delete=models.CASCADE, related_name='gig_event'
+        )
+    venue = models.ForeignKey(
+        Venue, on_delete=models.CASCADE, related_name='gig_venue'
+    )
+    fees = models.DecimalField('Total fees', max_digits=10, decimal_places=2)
+    is_payed = models.BooleanField(default=False)
+    notes = models.TextField(blank=True)
+
+    @property
+    def days_to(self):
+        """Function to return how many days
+        until the gig"""
+
+        today = date.today()
+        days_till_gig = (self.date - today)
+        return days_till_gig
+
+    class Meta:
+        ordering = ['-date']
+
+    def __str__(self):
+        return f"Gig on {self.date} at {self.venue.name} assined to {self.dj.user_name}"
