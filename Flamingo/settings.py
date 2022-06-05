d32f8a72d8316e30665ba3a987ced4844d1583a1
Dabronzo <ayrtonterceiro@gmail.com>
2022-06-05 17:11:10 +0000

    Implemented a few tweaks on the layout

--- Flamingo/settings.py
+++ Flamingo/settings.py
@@ -97,16 +97,16 @@ WSGI_APPLICATION = 'Flamingo.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
-# }
-
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 
 # Password validation
